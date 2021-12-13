15c0b9ea6991c062fd25c1298ecf5eb0f2c5f774
Emmanuelle du Colombier <emmanuelle.ducolombier@gmail.com>
2021-12-13 18:40:32 +0100

    exo en cours

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
-Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
-Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+require 'open-uri'
+require 'json'
+
+URL = "https://api.themoviedb.org/3/movie/top_rated?api_key=dfb68040430447137be70cab875f8f19&language=en-US&page=1"
+movies_serialized = URI.open(URL).read
+movies = JSON.parse(movies_serialized)
+
+Bookmark.destroy_all
+List.destroy_all
+Movie.destroy_all
+
+i = 0
+while i < 10
+  movie = Movie.create!(title: movies["results"][i]["title"],
+                      overview: movies["results"][i]["overview"],
+                      poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg",
+                      rating: movies["results"][i]["vote_average"])
+  list = List.create!(name: movies["results"][i]["release_date"])
+  Bookmark.create!(comment: "this is a test for a bookmark", movie_id: movie.id, list_id: list.id)
+  i += 1
+end
